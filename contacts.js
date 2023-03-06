ed66fb14080ac83f0a3b0064e1160d31b3ffd8c3
MishynaIra <gortenzia1987@gmail.com>
2023-03-06 11:00:07 +0200

    yargs import and use

--- contacts.js
+++ contacts.js
@@ -1,39 +1,20 @@
 //импорт модулей fs и path для работы с файловой системой
 const path = require('path');
 const fs = require('fs');
+const colors = require('colors');
 
 //Создай переменную contactsPath и запиши в нее путь к файлу contacts.json.
 // Для составления пути используй методы модуля path
 const contactsPath = path.resolve('./db/contacts.json');
 
 
-//Чтение файла + //Запись файла
-// В функциях используй модуль fs и его методы readFile() и writeFile()
-//Первым аргументом метода "readFileSync" передаётся абсолютный или относительный
-// адрес файла, который необходимо считать. Второй параметр - это кодировка файла для считывания.
-// fs.readFile('db/contacts.json', 'utf8', (error, contactsFile) => {
-//     if (error) throw error; // ошибка чтения файла, если есть
-//     console.log(contactsFile);// содержимое файла
-//
-//     //Первым параметром этой функции передаётся название файла, вторым данные для записи.
-//     // При этом в третьем параметре необходимо передать callback функцию
-//     // с единственным параметром - переменной, в которую запишется ошибка.
-//     let toWrite = contactsFile + "Mishyna Ira";
-//
-//     fs.writeFile('db/contacts.json', toWrite, (error) => {
-//         if (error) throw error;// ошибка чтения файла, если есть
-//         console.log('Contact was added in the file');
-//     });
-// });
 
 function listContacts() {
     fs.readFile(contactsPath, 'utf8', (error, contactsFile) => {
         if (error) throw error; // ошибка чтения файла, если есть
-        console.log(contactsFile);// содержимое файла
+        console.table(contactsFile.green);// содержимое файла
     })
 }
-
-
 
 function getContactById(contactId) {
     fs.readFile(contactsPath, 'utf8', (error, contactsFile) => {
@@ -50,13 +31,10 @@ function getContactById(contactId) {
         if (Object.keys(result).length) {
             console.log(result);
         } else {
-            console.log('Контакт с переданным id не существует');
+            console.log('Контакт с переданным id не существует'.red);
         }
     })
 }
-
-
-
 
 function removeContact(contactId) {
     fs.readFile(contactsPath, 'utf8', (error, contactsFile) => {
@@ -66,14 +44,14 @@ function removeContact(contactId) {
         const index = data.findIndex((item) => Number(item.id) === contactId);
 
         if (index === -1) {
-            return console.log('Елемента с таким id не существует');
+            return console.log('Елемента с таким id не существует'.red);
         }
         data.splice(index, 1);
 
         fs.writeFile('db/contacts.json', JSON.stringify(data), (error) => {
             if (error) throw error;// ошибка чтения файла, если есть
         });
-        console.log(`Contact with id ${contactId} was deleted successfully`);
+        console.log(`Contact with id ${contactId} was deleted successfully`.yellow);
     })
 }
 
@@ -102,7 +80,7 @@ function addContact(name, email, phone) {
       fs.writeFile(contactsPath, JSON.stringify(data), (error) => {
           if (error) throw error;// ошибка чтения файла, если есть
       });
-      console.log(`Contact was added successfully`);
+      console.log(`Contact was added successfully`.yellow);
   })
 }
 
--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-const moduleContacts = require('./contacts');
+// const moduleContacts = require('./contacts');
+const yargs = require("yargs");
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
 
-moduleContacts.listContacts();
-moduleContacts.getContactById(7);
-moduleContacts.removeContact(5);
-moduleContacts.addContact('Iryna', 'gortenzia@gmail.com', '063-077-11-62');
 
+const {listContacts, getContactById, addContact, removeContact} = require("./contacts");
+
+// moduleContacts.listContacts();
+// moduleContacts.getContactById(7);
+// moduleContacts.removeContact(5);
+// moduleContacts.addContact('Iryna', 'gortenzia@gmail.com', '063-077-11-62');
+
+
+
+function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case "list":
+            console.table(listContacts());
+            break;
+
+        case "get":
+            console.log(getContactById(id));
+            break;
+
+        case "add":
+            console.log(addContact(name,email,phone));
+            break;
+
+        case "remove":
+           console.log(removeContact(id));
+            break;
+
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
+}
+
+invokeAction(argv);
